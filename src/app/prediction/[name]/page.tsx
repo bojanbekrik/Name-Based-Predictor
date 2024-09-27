b9068fea01eeae6d7db2a291013b6f6fbd74df29
Bojan Bekrikj <bojanbekrik@gmail.com>
2024-09-27 13:37:15 +0200

    Initial commit

---
+++ src/app/prediction/[name]/page.tsx
@@ -0,0 +1,43 @@
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io/?name=${name}`);
+
+  return res.json();
+}
+
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io/?name=${name}`);
+
+  return res.json();
+}
+
+const getPredictedCountry = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io/?name=${name}`);
+
+  return res.json();
+}
+
+interface Params {
+  params: {name: string};
+}
+
+export default async function Page({params}: Params) {
+
+  const ageData = getPredictedAge(params.name);
+
+  const genderData = getPredictedGender(params.name);
+
+  const countryData = getPredictedCountry(params.name);
+
+  const [age, gender, country] = await Promise.all([ageData, genderData, countryData]); // age, country, data is actually the data that we are fetching
+
+  return (
+    <div>
+      <div>
+        <div>Personal info</div>
+        <div>Age: {age?.age}</div>
+        <div>Country: {country?.country[0]?.country_id}</div>
+        <div>Gender: {gender?.gender}</div>
+      </div>
+    </div>
+  );
+}
